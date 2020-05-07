6ddb33c8b95f518d1cc972b78e41933567982b3f
rahulaVT <rahula@vt.edu>
2020-05-06 20:06:21 -0400

    hello app using sql-alchemy, understanding ORM

---
+++ flask_hello_app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+## dialect://username:password@host:port/dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:myPassword@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db instance
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    __tablename_ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return '<Person ID:' + str(self.id) + ', name: ' + self.name + '>'
+
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    person = Person.query.first()
+    return 'Hello ' + person.name
