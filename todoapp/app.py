ba1c1081d92a1a89cdeaba589d31c10e37d27fcd
rahulaVT <rahula@vt.edu>
2020-05-12 00:33:08 -0400

    adding db migrationsFalse

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:myPassword@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return '<Person ID:' + str(self.id) + ', name: ' + self.description + '>'
 
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
@@ -49,8 +53,6 @@ def create():
         db.session.close()
         if not error:
             return jsonify(body)
-
-
 
     # return redirect(url_for('index'))
     # return jsonify({'description': todo.description})
